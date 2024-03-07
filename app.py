ba75ad687a931467006908b0d59ff228d82d4cf8
Urbano Villanueva <urbano.villanueva@autentia.com>
2024-03-07 19:39:34 +0100

    Reading file and saving in server

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.datastructures import CombinedMultiDict
+from werkzeug.utils import secure_filename
+
 from file_import.movement_file_form import MovementFileForm
 from flask_babel import Babel
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'NOBODY_KNOWS'
+app.config['UPLOAD_FOLDER'] = '/Users/urbano.villanueva/PycharmProjects/FinMapper/upload'
 
 
 def get_locale():
@@ -20,13 +26,15 @@ def hello_world():
 
 @app.route('/load', methods=['GET', 'POST'])
 def load():
-    if request.method == 'POST':
-        print(request)
-        return render_template('file_import/load_file.html')
+    form = MovementFileForm(CombinedMultiDict((request.files, request.form)))
 
-    if request.method == 'GET':
-        form = MovementFileForm()
-        return render_template('file_import/load_file.html', form = form)
+    if form.validate_on_submit():
+        data_file = form.file.data
+        filename = secure_filename(data_file.filename)
+        data_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('load'))
+
+    return render_template('file_import/load_file.html', form=form)
 
 
 if __name__ == '__main__':
