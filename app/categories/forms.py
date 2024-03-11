666ba2457929b0f42cf45e6c9eb763ec5f9507c0
Urbano Villanueva <urbano.villanueva@autentia.com>
2024-03-11 20:38:28 +0100

    Dashboard front working (no db writting)

---
+++ app/categories/forms.py
@@ -0,0 +1,10 @@
+from flask_babel import lazy_gettext
+from flask_wtf import FlaskForm
+from wtforms.fields.choices import SelectField
+from wtforms.fields.simple import StringField
+from wtforms.validators import DataRequired
+
+
+class NewCategoryForm(FlaskForm):
+    name = StringField(lazy_gettext('Name'), validators=[DataRequired()], render_kw={"class": "form-control"})
+    description = StringField(lazy_gettext('Description'), validators=[DataRequired()], render_kw={"class": "form-control"})
--- app/categories/routes.py
+++ app/categories/routes.py
@@ -1,8 +1,41 @@
-from flask import render_template
+from flask import render_template, request
 
 from app.categories import categories_blueprint
+from app.categories.domain.category import Category
+from app.categories.forms import NewCategoryForm
 
 
-@categories_blueprint.route('/categories/dashboard')
+def load_categories_from_db():
+    total_categories = 14
+    categories = []
+
+    for i in range(total_categories):
+        name = f"Categoría {i + 1}"
+        description = f"Descripción de la Categoría {i + 1}"
+        category = Category(name, description)
+        categories.append(category)
+
+    return categories
+
+
+@categories_blueprint.route('/categories/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    return render_template('categories/categories_dashboard.html')
+    categories = load_categories_from_db()
+    new_category_form = NewCategoryForm(request.form)
+
+    if request.method == 'POST':
+        form = NewCategoryForm(request.form)
+        if form.validate_on_submit():
+            new_category = Category(form.name.data, form.description.data)
+            categories.append(new_category)
+            new_category_form.name.data = None
+            new_category_form.description.data = None
+            # guardar categoría en base de datos
+            print('Se guarda la categoría en base de datos')
+            return render_template('categories/categories_dashboard.html',
+                                   categories=categories,
+                                   new_category_form=new_category_form)
+
+    return render_template('categories/categories_dashboard.html',
+                           categories=categories,
+                           new_category_form=new_category_form)
