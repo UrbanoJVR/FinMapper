22e2308aaee88d79694ffde9dbba4a5c1d57fd6a
Urbano Villanueva <urbano.villanueva@autentia.com>
2024-04-25 21:17:14 +0200

    Refactor categories routes

--- app/categories/routes.py
+++ app/categories/routes.py
@@ -1,3 +1,5 @@
+from urllib.request import Request
+
 from flask import render_template, request, redirect, url_for
 
 from app.categories import categories_blueprint
@@ -12,19 +14,24 @@ category_service = CategoryService(CategoryRepository())
 @categories_blueprint.route('/categories/dashboard', methods=['GET', 'POST'])
 def dashboard():
     categories = category_service.get_all_categories()
-    new_category_form = NewCategoryForm(request.form)
 
     if request.method == 'POST':
-        if new_category_form.validate_on_submit():
-            new_category = Category(
-                name=new_category_form.name.data,
-                description=new_category_form.description.data
-            )
-            category_service.save_category(new_category)
-            new_category_form.name.data = None
-            new_category_form.description.data = None
-            return redirect(url_for('categories_blueprint.dashboard'))
+        create_category(request)
+        return redirect(url_for('categories_blueprint.dashboard'))
 
     return render_template('categories/categories_dashboard.html',
                            categories=categories,
-                           new_category_form=new_category_form)
+                           new_category_form=NewCategoryForm())
+
+
+def create_category(req):
+    new_category_form = NewCategoryForm(req.form)
+
+    if new_category_form.validate_on_submit():
+        new_category = Category(
+            name=new_category_form.name.data,
+            description=new_category_form.description.data
+        )
+        category_service.save_category(new_category)
+        new_category_form.name.data = None
+        new_category_form.description.data = None
