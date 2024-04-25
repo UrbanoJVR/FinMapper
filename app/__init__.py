6c33590becac9d9c392d9d356ccc0c9a336496f2
Urbano Villanueva <urbano.villanueva@autentia.com>
2024-04-25 11:12:30 +0200

    Using non-arguments app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Flask
 from flask_babel import Babel
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import config
 
@@ -19,14 +22,18 @@ def get_locale():
 
 babel = Babel()
 db = SQLAlchemy()
+migrate = Migrate()
 
 
-def create_app(config_name):
+def create_app():
     app = Flask(__name__)
+
+    config_name = os.getenv('APP_CONFIG') or 'default'
 
     babel.init_app(app, locale_selector=get_locale)
     app.config.from_object(config[config_name])
     db.init_app(app)
+    migrate.init_app(app, db)
     config[config_name].init_app(app)
     register_blueprints(app)
 
