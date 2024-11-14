464499d45c7d40238fa516a0dbe32df659cdea25
UrbanoVJR <urbanovr@ext.inditex.com>
2024-11-14 17:09:20 +0100

    revamp create category command handler tests

--- app/test/category/command/test_create_category_command_handler.py
+++ app/test/category/command/test_create_category_command_handler.py
@@ -1,35 +1,32 @@
+import unittest
 from unittest.mock import Mock
-
-import pytest
 
 from app.src.application.category.command.create_category_command import CreateCategoryCommand
 from app.src.application.category.command.create_category_command_handler import CreateCategoryCommandHandler
-from app.src.presentation.routes.category_routes import category_repository
+from app.src.infrastructure.repository.category_repository import CategoryRepository
 
-@pytest.fixture
-def mock_repository():
-    return Mock()
+class TestCreateCategoryCommandHandler(unittest.TestCase):
 
-@pytest.fixture
-def sut(mock_repository):
-    return CreateCategoryCommandHandler(category_repository=mock_repository)
+    def setUp(self):
+        self.mock_repository = Mock(spec=CategoryRepository)
+        self.sut = CreateCategoryCommandHandler(category_repository=self.mock_repository)
 
-def test_execute_success(sut, mock_repository):
-    command = CreateCategoryCommand(name='Name', description='Description')
-    mock_repository.exists_by_name.return_value = False
+    def test_execute_success(self):
+        command = CreateCategoryCommand(name='Name', description='Description')
+        self.mock_repository.exists_by_name.return_value = False
 
-    result = sut.execute(command)
+        result = self.sut.execute(command)
 
-    assert result is True
-    mock_repository.exists_by_name.assert_called_once_with(command.name)
-    mock_repository.save.assert_called_once()
+        assert result is True
+        self.mock_repository.exists_by_name.assert_called_once_with(command.name)
+        self.mock_repository.save.assert_called_once()
 
-def test_execute_when_category_name_exists(sut, mock_repository):
-    command = CreateCategoryCommand(name='Name', description='Description')
-    mock_repository.exists_by_name.return_value = True
+    def test_not_create_when_category_name_exists(self):
+        command = CreateCategoryCommand(name='Name', description='Description')
+        self.mock_repository.exists_by_name.return_value = True
 
-    result = sut.execute(command)
+        result = self.sut.execute(command)
 
-    assert result is False
-    mock_repository.exists_by_name.assert_called_once_with(command.name)
-    mock_repository.save.assert_not_called()
+        assert result is False
+        self.mock_repository.exists_by_name.assert_called_once_with(command.name)
+        self.mock_repository.save.assert_not_called()
